6211e90a992693986d8d36a07c1d1c19c2a83b29
Almanac <ruoyw0@gmail.com>
2021-02-11 08:40:36 -0800

    Adding in basic game flow components. For now just start game and set everything to set state to Exit the game

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    enum GameState
+    {
+        SelectMode,
+        SelectRounds,
+        TakeTurn,
+        GameOver,
+        Exit
+    }
+    class Game
+    {
+        private List<Gesture> gestures = new List<Gesture>();
+        private int roundsToWin;
+
+        // Handles all of the game logic
+        public void RunGame()
+        {
+            GameState state = GameState.SelectMode;
+            while (state != GameState.Exit)
+            {
+                switch (state)
+                {
+                    case GameState.SelectMode:
+                        state = SelectMode();
+                        break;
+                    case GameState.SelectRounds:
+                        state = SelectRounds();
+                        break;
+                    case GameState.TakeTurn:
+                        state = TakeTurn();
+                        break;
+                    case GameState.GameOver:
+                        state = GameOver();
+                        break;
+                    default:
+                        state = GameState.Exit;
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public GameState SelectMode()
+        {
+            Console.WriteLine("Starting Game");
+            return GameState.Exit;
+        }
+        public GameState SelectRounds()
+        {
+            return GameState.Exit;
+        }
+        public GameState TakeTurn()
+        {
+            return GameState.Exit;
+        }
+        public GameState GameOver()
+        {
+            return GameState.Exit;
+        }
+
+    }
+}
